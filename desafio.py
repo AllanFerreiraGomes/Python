59c2ee60cbb945b5af23835f997fc45683e97d25
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2023-05-24 22:16:27 -0300

    aa

---
+++ desafio.py
@@ -0,0 +1,22 @@
+
+
+def main():
+ te =  float(input("temperatura externa"))
+ ti = float(input("temperatura Interna"))
+ umidade =  float(input("Porcentagem da Humidade"))
+#   print(te)
+#   print(ti)
+#  print(umidade)
+
+def botao_ligar():
+    botao = input("Estado")
+    if botao == "ligar":
+       print("Ligado")
+    else  :
+       print("Desligado")
+
+
+
+
+main()
+botao_ligar()
